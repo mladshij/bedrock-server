769db93c6bd017f52eeba0733f35e975f99b78ce
mladshij <alex@vorotyntsev.name>
2019-03-23 20:55:13 +0700

    Version 1.10 + new ports

--- minecraft_launcher.sh
+++ minecraft_launcher.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
 server=$1
-base_dir="/var/games"
+base_dir="/var/games/MCPE"
 work_dir="${base_dir}"/"${server}"
 
 mkdir -p "${work_dir}"
 touch "${work_dir}"/whitelist.json
 touch "${work_dir}"/server.properties
-touch "${work_dir}"/ops.json
+touch "${work_dir}"/permissions.json
 
-docker run -d -h ${server} --name=${server}\
+docker run -d -p 19132-19133:19132-19133/udp -h ${server} --name=mc-${server}\
  -v "${work_dir}/whitelist.json:/opt/minecraft/whitelist.json:z"\
  -v "${work_dir}/server.properties:/opt/minecraft/server.properties:z"\
- -v "${work_dir}/ops.json:/opt/minecraft2/ops.json:z"\
+ -v "${work_dir}/permissions.json:/opt/minecraft/permissions.json:z"\
  -v "${work_dir}/worlds:/opt/minecraft/worlds:z"\
- --network=host\
- imetzach/minecraft-bedrockserver
+ mladshij/minecraft-bedrockserver
